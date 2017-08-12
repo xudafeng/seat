d46015fe94a178f652c98230a65f9f063e7a7b2f
xudafeng <xudafeng@126.com>
2017-08-12 11:05:22 +0800

    tweak project

--- template/lib/<#=name#>.js
+++ template/lib/<#=name#>.js
@@ -1,14 +1,1 @@
-/* ================================================================
- * <#=name#> by xdf(xudafeng[at]126.com)
- *
- * first created at : <#=time#>
- *
- * ================================================================
- * Copyright <#year#> xdf
- *
- * Licensed under the MIT License
- * You may not use this file except in compliance with the License.
- *
- * ================================================================ */
-
 'use strict';
